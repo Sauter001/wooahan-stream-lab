dc638c6772afdc79c0318628d529b268d82637e6
sauter001 <lsj457@naver.com>
2025-11-23 16:25:13 +0900

    feat: add level2 stage

--- src/main/java/solutions/level2/Level2.java
+++ src/main/java/solutions/level2/Level2.java
@@ -1,4 +1,23 @@
 package solutions.level2;
 
+import java.util.List;
+
 public class Level2 {
+    public static List<String> getGuildMembersAboveLevel30(List<Character> characters) {
+        throw new UnsupportedOperationException("구현 안됨");
+    }
+
+    public static double getTop10AverageGold(List<Character> characters) {
+        throw new UnsupportedOperationException("구현 안됨");
+    }
+
+    public static String getAllItemNamesJoined(List<Character> characters) {
+        throw new UnsupportedOperationException("구현 안됨");
+    }
+
+    public static List<String> getSilverTrophyWinners(List<Character> characters) {
+        throw new UnsupportedOperationException("구현 안됨");
+    }
+
+
 }
